fb9c067d0e6999104b4a60ccd2f3b084cb340071
JamesLeggero <james.m.leggero@gmail.com>
2020-09-21 23:49:17 -0400

    matchr first draft

---
+++ challenges/rb_react_matchr/matchr_client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Daters from './components/Hello'
+
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <div className='container'>
+      <h1>Hello Daters!</h1>
+      <Daters />
+      </div>
+    </div>
+  );
+}
+
+export default App;
