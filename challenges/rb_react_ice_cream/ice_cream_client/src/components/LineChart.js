747514d90dd4f29e00372a000d174681c808257f
JamesLeggero <james.m.leggero@gmail.com>
2020-09-22 19:56:24 -0400

    fetch request and uE working in console

--- challenges/rb_react_ice_cream/ice_cream_client/src/components/LineChart.js
+++ challenges/rb_react_ice_cream/ice_cream_client/src/components/LineChart.js
@@ -3,6 +3,20 @@ import Chart from 'chart.js'
 
 export default function LineChart(props){
 
+    const getData = async () => {
+        try {
+            const response = await fetch('/ice_creams/1')
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return(
         <>
         <h1>Ice Cream</h1>
