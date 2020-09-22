81bdf0d753a8d2b7e6a346b2a2e1428c92767cf2
JamesLeggero <james.m.leggero@gmail.com>
2020-09-22 19:29:48 -0400

    front end showing up

---
+++ challenges/rb_react_ice_cream/ice_cream_client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LineChart from './components/LineChart'
+
+function App() {
+  return (
+    <div className="App">
+      <LineChart />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ challenges/rb_react_ice_cream/ice_cream_client/src/components/LineChart.js
@@ -0,0 +1,11 @@
+import React, {useEffect } from 'react';
+import Chart from 'chart.js'
+
+export default function LineChart(props){
+
+    return(
+        <>
+        <h1>Ice Cream</h1>
+        </>
+    )
+}
