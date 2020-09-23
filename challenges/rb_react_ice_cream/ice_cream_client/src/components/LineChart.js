7abf90464d7e030b69447d80b0f31ba5004289a8
JamesLeggero <james.m.leggero@gmail.com>
2020-09-22 20:29:07 -0400

    MVP commit

--- challenges/rb_react_ice_cream/ice_cream_client/src/components/LineChart.js
+++ challenges/rb_react_ice_cream/ice_cream_client/src/components/LineChart.js
@@ -3,11 +3,38 @@ import Chart from 'chart.js'
 
 export default function LineChart(props){
 
+    const prepareData = data => {
+        const chartData = {
+            labels: [],
+            datasets: [{
+                label: 'Meatball Pints Sold',
+                data: []
+            },
+        ]
+        }
+        data.pints.forEach(pint => {
+            chartData.labels.push(pint.month)
+            chartData.datasets[0].data.push(pint.pints_sold)
+        })
+        return chartData
+    }
+
+    const createChart = data => {
+        const ctx = document.querySelector('#pints');
+        const pintsChart = new Chart(ctx, {
+            type: "line",
+            data: data
+        })
+    }
+
     const getData = async () => {
         try {
             const response = await fetch('/ice_creams/1')
             const data = await response.json()
-            console.log(data)
+            // console.log(data)
+            const jData = prepareData(data)
+            console.log(jData)
+            createChart(jData)
         } catch (error) {
             console.error(error)
         }
@@ -20,6 +47,7 @@ export default function LineChart(props){
     return(
         <>
         <h1>Ice Cream</h1>
+        <canvas id="pints" width="300" height="100"></canvas>
         </>
     )
 }
