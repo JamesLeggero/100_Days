4eb9c3d87812d4e02fd98cc82b164be1c0705cae
JamesLeggero <james.m.leggero@gmail.com>
2020-09-23 21:36:35 -0400

    recipe join table added

--- projects/cookies/cookies_api/db/schema.rb
+++ projects/cookies/cookies_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_011610) do
+ActiveRecord::Schema.define(version: 2020_09_24_012834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_09_24_011610) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "recipe"
+    t.bigint "user_id", null: false
+    t.bigint "cookie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cookie_id"], name: "index_recipes_on_cookie_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "sig_rec"
@@ -29,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_09_24_011610) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recipes", "cookies", column: "cookie_id"
+  add_foreign_key "recipes", "users"
 end
--- projects/cookies/cookies_api/db/seeds.rb
+++ projects/cookies/cookies_api/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+    { name: "James Leggero", sig_rec: 1 },
+    { name: "John Davis", sig_rec: 5 },
+    { name: "Alex Yanovich", sig_rec: 3 },
+    { name: "Cody Howard", sig_rec: 4 },
+    { name: "Arthur Bernier Jr", sig_rec: 2 },
+    { name: "Ira Herman", sig_rec: 8 },
+    { name: "Ayla Ex", sig_rec: 6 },
+    { name: "Paul Chapman", sig_rec: 7 }
+])
+
+Cookie.create([
+    { name: "Chocolate Chip", nuts: true },
+    { name: "Macadamia", nuts: true },
+    { name: "Sugar", nuts: false },
+    { name: "Lemon", nuts: false },
+    { name: "Oatmeal Raisin", nuts: true },
+    { name: "Gingerbread", nuts: false },
+    { name: "Peanut Butter", nuts: true },
+    { name: "Raspberry Thumbprint", nuts: false },
+])
