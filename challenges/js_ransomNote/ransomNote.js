dab6ce664dd934923f2e119e5a2fefacc6a71f8a
JamesLeggero <james.m.leggero@gmail.com>
2020-10-08 19:11:39 -0400

    17/22 passed

---
+++ challenges/js_ransomNote/ransomNote.js
@@ -0,0 +1,100 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+
+    function inMag(word) {
+        for (let i = 0; i < magazine.length; i++) {
+        if (word === magazine[i]) {
+            return (magazine.indexOf(magazine[i]))
+            }
+            
+        }
+        return false
+    }
+
+
+    // for (let i = 0; i < note.length; i++) {
+    //     if (note.length === 0) {
+    //         console.log('Yes')
+    //         return
+    //     } else if (!inMag(note[i]) === true){
+    //         console.log('No')
+    //         return
+    //     } else {
+    //         note.splice(note[i], 1)
+    //         magazine.splice(inMag(note[i]), 1)
+    //         checkMagazine(magazine, note)
+    //     }
+        
+        
+    // }
+
+    function noteTest(words) {
+
+        for (let i = 0; i < words.length; i++){
+            if (inMag(words[i]) === false) {
+                // console.log(words[i], 'not found in mag')
+                console.log('No')
+                return
+            } else {
+                magazine.splice(inMag(words[i]), 1)  
+            }
+            
+        }
+            console.log('Yes')
+        return
+
+    }
+
+    noteTest(note)
+
+    
+    
+
+
+    
+
+    
+
+
+    
+
+
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
