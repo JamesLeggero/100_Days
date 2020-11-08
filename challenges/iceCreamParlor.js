78d3eb17fc779bdb1584383ff91cef9bd67fd6ee
JamesLeggero <james.m.leggero@gmail.com>
2020-11-07 21:27:23 -0500

    iCP A

---
+++ challenges/iceCreamParlor.js
@@ -0,0 +1,58 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the whatFlavors function below.
+function whatFlavors(cost, money) {
+    cost.unshift(0)
+    const flavors = []
+    for (let i = 1; i < cost.length; i++){
+        for (let j = 1; j < cost.length; j++) {
+            if (i === j) {continue}
+            else if (cost[i] + cost[j] === money){
+            flavors.push(i)
+            flavors.push(j)
+            flavors.sort((a, b) => {return a - b})
+            break
+            }
+        }
+    }
+    
+    console.log(flavors[0], flavors[1])
+
+
+}
+
+function main() {
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const money = parseInt(readLine(), 10);
+
+        const n = parseInt(readLine(), 10);
+
+        const cost = readLine().split(' ').map(costTemp => parseInt(costTemp, 10));
+
+        whatFlavors(cost, money);
+    }
+}
