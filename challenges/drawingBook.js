96ca6b86881f77572bfac2ded1e479bd01821f61
JamesLeggero <james.m.leggero@gmail.com>
2020-12-23 15:27:43 -0500

    drawing book not yet

---
+++ challenges/drawingBook.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+function pageCount(n, p) {
+    let fromFront = 0
+    let fromBack = 0
+    if (p / n < 0.5) {
+        //from front
+        return ((n/2) / p)
+    }
+    return (((n - p) / 2) / p)
+    
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = parseInt(readLine(), 10);
+
+    let result = pageCount(n, p);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
